8aabf5b344776b9600bfcda4fe865e961a44f87a
Nathan L. Walls <nathan@wallscorp.us>
2022-05-19 21:41:13 -0400

    fixup! Adds articles

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,4 +1,15 @@
 class Article < ApplicationRecord
   belongs_to :user
   belongs_to :site
+
+  validates(
+    :slug,
+    length: {maximum: 250},
+    presence: true,
+    uniqueness: {case_sensitive: false}
+  )
+  validates(
+    :body,
+    presence: true
+  )
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,23 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_04_19_011053) do
+ActiveRecord::Schema[7.0].define(version: 2022_05_19_011752) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "articles", force: :cascade do |t|
+    t.string "slug", null: false
+    t.string "title"
+    t.text "summary"
+    t.text "body", null: false
+    t.bigint "user_id", null: false
+    t.bigint "site_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["site_id"], name: "index_articles_on_site_id"
+    t.index ["slug"], name: "index_articles_on_slug"
+    t.index ["user_id"], name: "index_articles_on_user_id"
+  end
 
   create_table "sites", force: :cascade do |t|
     t.string "name", null: false
@@ -38,4 +52,6 @@ ActiveRecord::Schema[7.0].define(version: 2022_04_19_011053) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "articles", "sites"
+  add_foreign_key "articles", "users"
 end
