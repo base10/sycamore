3e483254b2b736e7e75b159f00730fe054296c97
Nathan L. Walls <nathan@wallscorp.us>
2022-05-22 22:31:45 -0400

    fixup! Adds articles

--- app/models/article.rb
+++ app/models/article.rb
@@ -9,10 +9,15 @@ class Article < ApplicationRecord
     presence: true,
     uniqueness: {case_sensitive: false}
   )
-  validates(
-    :body,
-    presence: true
-  )
+  validates(:body, presence: true)
+
+  validates(:published_on, presence: true)
+
+  scope :published, -> { where(published: true) }
+  scope :unpublished, -> { where(published: false)}
+  scope :private , -> { where(private: true) }
+  scope :public, -> { where(private: false) }
+
 
   # Add scope for published
   # Add scope for unpublished
